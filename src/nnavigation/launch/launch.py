a4ff43ee16230cb3c619719eefd4b3954f563a75
Ferdinand <ferdinand.hartmann@web.de>
2025-05-05 17:48:35 +0200

    sim testing but frames are wrong

--- src/nnavigation/launch/launch.py
+++ src/nnavigation/launch/launch.py
@@ -1,14 +1,58 @@
 from launch import LaunchDescription
+from launch.substitutions import Command
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 import os
+from launch_ros.substitutions import FindPackageShare
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import FindExecutable
 
 def generate_launch_description():
 
     package_name = 'nnavigation'
+
+
+
+    with open(
+        os.path.join(
+            get_package_share_directory('turtlebot3_description'),
+            'urdf',
+            'turtlebot3_waffle2.urdf'
+        ), 'r') as infp:
+        urdf_content = infp.read()
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': urdf_content}, 
+                    {'use_sim_time': True}],
+        remappings=[('/joint_states', '/waffle2/joint_states')]
+    )
+
+
+
+    alias_base_link = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='base_link_alias',
+        arguments=['0', '0', '0', '0', '0', '0', 'base_link', 'waffle2/base_link'],
+        output='screen',
+        parameters=[{'use_sim_time': True}]
+    )
+
+    static_odom_to_basefootprint = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        arguments=['0', '0', '0', '0', '0', '0', 'waffle2/odom', 'waffle2/base_footprint'],
+        output='screen',
+        parameters=[{'use_sim_time': True}]
+    )
+
+
 
     map_file = PathJoinSubstitution([
         get_package_share_directory(package_name),
@@ -19,7 +63,7 @@ def generate_launch_description():
     params_path = os.path.join(
         get_package_share_directory(package_name),
         'config',
-        'nav2_params2.yaml'
+        'nav2_params2_2_sim.yaml'
     )
 
     rviz_config = os.path.join(
@@ -40,12 +84,14 @@ def generate_launch_description():
         name='rviz2',
         output='screen',
         arguments=['-d', rviz_config],
+        parameters=[{'use_sim_time': True}]
     )
 
     static1 = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        arguments=['0', '0', '0', '0', '0', '0', 'map', 'waffle2/odom']
+        arguments=['0', '0', '0', '0', '0', '0', 'map', 'waffle2/odom'],
+        parameters=[{'use_sim_time': True}]
     )
 
     # map_server_node = TimerAction(
@@ -67,16 +113,17 @@ def generate_launch_description():
 
     return LaunchDescription([
         rviz_node,
+        robot_state_publisher_node,
+        alias_base_link,
+        static_odom_to_basefootprint,
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(bringup_launch),
             launch_arguments={
                 'map': map_file,
                 'params_file': params_path,
-                'use_sim_time': 'false'
+                'use_sim_time': 'true'
             }.items()
         ),
-
-        
         # robot_state_publisher,
         static1
                 ])
